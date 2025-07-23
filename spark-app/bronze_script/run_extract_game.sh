05f249d97af616f88bd3a7dafdd8882f04955c54
unknown <hamegy08lleo@gmail.com>
2025-07-23 11:51:52 +0700

    updating extract game

--- spark-app/bronze_script/run_extract_game.sh
+++ spark-app/bronze_script/run_extract_game.sh
@@ -4,4 +4,5 @@ spark-submit \
   --jars $(echo /opt/spark/jars/*.jar | tr ' ' ',') \
   --master spark://spark-master:7077 \
   --deploy-mode client \
-  /opt/spark-app/bronze_script/extract_game.py
+  /opt/spark-app/bronze_script/extract_game.py \
+  --day $1
